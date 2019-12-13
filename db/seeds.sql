44ade879bc38f50b16f1a1ea4a2223dd69b1cdbd
STERRENBERG, MARK <mark.allen.sterrenberg@gmail.com>
2019-12-12 23:28:05 -0600

    setting up layout for the project and adding configurations where needed

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marben, false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Allens, true');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Origin, false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Plate, false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Union, true');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Golden Star, true');
